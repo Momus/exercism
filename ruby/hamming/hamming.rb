9a826ca23656f89f1fef227c08ac742065aeb9cd
Dmitri G. Brengauz <dmitri@momus.net>
2015-05-23 13:35:59 -0600

    Moved "Microscheme" to module Still not able to be accessed.

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,57 +1,49 @@
 
-#Should start by declaring a class, but if a Hamming object will never
-#be instantiated, shouldn't this be a module instead?
 
+=begin
+In this iteration, I got rid of my attempts to define Scheme-like
+functions, however, the counter is still not counting.  Will tackle
+again tomorrow, but if someone can spot my error, hints will be apprecaited!!!!
+
+Dmitri
+
+=end
+
+  ##Disguising Ruby strings as Scheme lists
+
+=begin
+#I want to be able to use these functions, but I can't call them!
+module MicroScheme
+    
+  def r_car(list_like_object)
+    list_like_object[0]
+  end
+  
+  def r_cdr(list_like_object)
+    list_like_object = list_like_object[1..-1] if list_like_object
+  end
+
+end
+=end
 
 class Hamming
 
-=begin
-What I wanted to do is solve this problem using the same sort
-of recursion that I saw in the Little Schemer book, because this
-problem really seem to fit that pattern.  Instead, I got completely
-stuck.
-
-I used the lambda because without it the methods became out of scope
-for the block.  However, I can't seem to get the counter to count as
-the block iterates.  I've tried referencing it and returning it in
-different places in the blocks.  I tried to make it a class variable,
-even a global variable to see if scope was the problem. 
-
-I've been staring at this for hours, so if someone would nudge me in
-the right direction, I would be very grateful. 
-
-Dmitri
-=end
-
-  ##Disguising Ruby strings as Scheme lists 
-  def s_car(string)
-    string[0]
-  end
-  
-  def s_cdr(string)
-    string = string[1..-1] if string
-  end
-
-   
   def self.compute(first_sequence, second_sequence)
-      if first_sequence.length  != second_sequence.length
-        raise ArgumentError, 'Hamming number only defined for sequences of same length'
-      else
-        counter = 0
-        comp =  lambda do
-          if s_car(first_sequence) != s_car(second_sequence)
-            counter += 1
-          end
-          first_sequence  = s_cdr(first_sequence) 
-          second_sequence  = s_cdr(second_sequence)
-          if first_sequence
-            comp(first_sequence, second_sequence)
-          end
-        
-        end
-        return counter
-      end
-      
-  end
+    result_string = "" 
+    #   include MicroScheme
+    if first_sequence.length  != second_sequence.length
+      raise ArgumentError, 'Hamming number only defined for sequences of same length'
+    else
+      loop  do
+        result_string =  first_sequence[0]  if first_sequence[0]  != second_sequence[0]
+        first_sequence[0]  = second_sequence[0]  = ""
+        break first_sequence.empty?
+      end 
+    end
+    
+    result_string.length
+
+  end #compute
+  
   
 end #class Hamming
