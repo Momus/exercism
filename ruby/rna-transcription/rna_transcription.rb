04b736ef2b55a9e0baf66805dc0c92c577b82367
Dmitri G. Brengauz <dmitri@momus.net>
2015-05-30 13:07:43 -0600

    Could not resist another version of rna_transcription.rb

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,35 +1,26 @@
-class Complement
+=begin
+Stolen from @rmetzler who claims to have stole it from others.
+=end
 
-  Complement_Map = { dna_to_rna:  {
-                       'A' => 'U' ,
-                       'T' => 'A' ,
-                       'G' => 'C' ,    
-                       'C' => 'G' ,
-                     } ,
-                     rna_to_dna: {
-                       'A' => 'T' ,
-                       'U' => 'A' ,
-                       'C' => 'G' ,
-                       'G' => 'C' ,    
-                     }
-                   }
+module Complement
+
+  DNA_nucleotide = "GCTA"
+  RNA_nucleoride = "CGAU"
   
-  def self.of_dna(dna_string)
-    generate_complement(:dna_to_rna, dna_string)
-  rescue
-    raise ArgumentError, "Uracil not present in DNA"
+  def self.of_dna(strand)
+    nucleotide_check("U", strand)
+    strand.tr DNA_nucleotide, RNA_nucleoride
   end
   
-  def self.of_rna(rna_string)
-    generate_complement(:rna_to_dna, rna_string)
- rescue
-    raise ArgumentError, "Thymine not present in RNA"
-  end
-  
- def self.generate_complement(map_index, nucleotide_string)
-   nucleotide_string.each_char.with_object("") { |base, complement| complement <<  Complement_Map[map_index][base]}
+  def self.of_rna(strand)
+    nucleotide_check("T", strand)
+    strand.tr RNA_nucleoride, DNA_nucleotide
   end
 
- private_class_method :generate_complement
+
+  def self.nucleotide_check(nucleotide, strand)
+    wrong_place = strand =~ /#{nucleotide}/
+    wrong_place ? raise(ArgumentError, "Wrong nucleotide in position #{(wrong_place + 1).to_s}") : strand
+  end
   
 end #class ComplementTest
