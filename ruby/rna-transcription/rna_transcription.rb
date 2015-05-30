8cfdce2f668746aea44217e56275388a914101a8
Dmitri G. Brengauz <dmitri@momus.net>
2015-05-29 18:12:21 -0600

    with_each object does a nice cleanup

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -13,34 +13,21 @@ class Complement
                        'G' => 'C' ,    
                      }
                    }
-
-
- 
-
   
   def self.of_dna(dna_string)
     generate_complement(:dna_to_rna, dna_string)
   rescue
     raise ArgumentError, "Uracil not present in DNA"
   end
-
   
   def self.of_rna(rna_string)
     generate_complement(:rna_to_dna, rna_string)
-  rescue
+ rescue
     raise ArgumentError, "Thymine not present in RNA"
   end
   
-  
-  
  def self.generate_complement(map_index, nucleotide_string)
-
-   # Ennumerate's #each_with_object throws no method errror with strings
-   # So a string object is provided:
-    complement = ""
-    nucleotide_string.each_char { |c| complement <<  Complement_Map[map_index][c]}
-    return complement
-  
+   nucleotide_string.each_char.with_object("") { |base, complement| complement <<  Complement_Map[map_index][base]}
   end
 
  private_class_method :generate_complement
