b29bcbfd16ab5215f86252025de4c71f7b4873fb
Dmitri G. Brengauz <dmitri@momus.net>
2015-05-30 00:02:05 -0600

    All tests passing raindrops First iteration

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,22 +2,11 @@
 
    def self.convert(number)
      outstring = ""
-     case
-     when number.modulo(3) == 0
-       count = 0
-       num = number
-       until num.modulo(3) != 0
-         count += 1
-         num = num / 3
-         outstring << "Pling"
-       end
-       outstring
-     else number.to_s
+     outstring << "Pling" if number.modulo(3) == 0 
+     outstring << "Plang" if number.modulo(5) == 0 
+     outstring << "Plong" if number.modulo(7) == 0
+     outstring[0] ? outstring : number.to_s
      end
-     
-        
-   end
-
  
   
  end #Raindrops
