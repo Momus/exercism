0058279e99a48e1963e07c4179e344a58ca9457d
Dmitri G. Brengauz <dmitri@momus.net>
2015-07-29 20:05:47 -0600

    Started Elisp.

---
+++ elisp/hello-world/hello-world.el
@@ -0,0 +1,18 @@
+;;; hello-world.el --- Hello World Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+
+(provide 'hello-world)
+
+(defun hello ()
+  ;; Function to say what needs
+  ;; to be said.
+  (print "Hello, World!")
+  )
+
+(hello)
+  
+;;; hello-world.el ends here
