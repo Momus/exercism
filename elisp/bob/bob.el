c5c9965748dd8b3470e5d2a0496b4d68030c55e8
Dmitri G. Brengauz <dmitri@momus.net>
2015-08-05 15:39:54 -0600

    First all GREEN for bob.el

--- elisp/bob/bob.el
+++ elisp/bob/bob.el
@@ -1,35 +1,49 @@
-;;; bob.el --- Bob exercise (exercism)
+;;; bob.el --- Bob exercise (exercise)
 
 ;;; Commentary:
 
 ;;; Code:
 
-
-(provide 'bob)
-
-(defun shouting-p (shout-string)
-  " Blackadder:: Tell me, do you ever stop shouting at the lower orders?
-    Duke of Wellington: NEVER! There's only one way to win a campaign: SHOUT, SHOUT AND SHOUT AGAIN!
-    Blackadder: You don't think inspired leadership and tactical ability has anything to do with it?
-    Duke of Wellington: (pause) NO! It's all down to shouting! BAAAH!"
-  (equal (upcase string) shout-string)
-  )
-
-(defun question-p (quest-string)
-  "Checking for interrogation"
-  (string-match-p "\?$" quest-string ))
-
-
-(defun bob (&optional bob-string)
-  "Say something stupid, get something stupid"
-  (if bob-string ((if (shouting-p bob-string)
-                      (message "Whoa, chill out!")
-                    (if question-p bob-string (message "Sure")
-                        (message "Whatever."))
-                    ))
-    (message "Fine. Be that way!")
+(defun nuthin-p (mutter-string)
+  "Better to stay silent and be thought a fool ..."
+  (if (null (string-match-p "[a-z0-9A-Z]" mutter-string))
+      t
+    nil
     )
   )
 
+(defun shout-p (shout-string)        
+  "It's all down to shouting! BAAAHH"
+   (and (string-match-p "[a-zA-Z]+" shout-string)  (equal (upcase shout-string) shout-string) )
+  )
+
+
+ (defun whuh-p (quest-string)
+   "A question is anything you want with a bang at the end."
+   (string-match-p "\?$" quest-string )
+   )
+
+
+(defun response-for (&optional bob-string)
+  "Say something stupid, get something stupid"
+
+  (let  ;This is Bob's vocab
+      ((b-shout "Whoa, chill out!")
+       (b-whuh  "Sure.")
+       (b-nuthin "Fine. Be that way!")
+       (b-generic "Whatever.")
+       )
+    
+    (cond ; Let body is what he does with it
+     ((nuthin-p bob-string) (message b-nuthin))
+     ((shout-p bob-string) (message b-shout))
+     ((whuh-p bob-string) (message b-whuh))
+     ((message b-generic))
+     )
+    )
+  )
+
+
+(provide 'bob)
 
 ;;; bob.el ends here
