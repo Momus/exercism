1a4283a99be2287f7a2982df66ccce4a84a40d83
Dmitri G. Brengauz <dmitri@momus.net>
2015-08-05 21:16:54 -0600

    GREEN and ready for refactor

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -4,23 +4,27 @@ Stolen from @rmetzler who claims to have stole it from others.
 
 module Complement
 
-  DNA_nucleotide = "GCTA"
-  RNA_nucleoride = "CGAU"
+  DNA_NUCLEOTIDES = "GCTA"
+  RNA_NUCLEOTIDES = "CGAU"
   
   def self.of_dna(strand)
     nucleotide_check("U", strand)
-    strand.tr DNA_nucleotide, RNA_nucleoride
+    strand.tr DNA_NUCLEOTIDES, RNA_NUCLEOTIDES
   end
   
   def self.of_rna(strand)
     nucleotide_check("T", strand)
-    strand.tr RNA_nucleoride, DNA_nucleotide
+    strand.tr RNA_NUCLEOTIDES, DNA_NUCLEOTIDES
   end
 
-
+  private
+  
   def self.nucleotide_check(nucleotide, strand)
     wrong_place = strand =~ /#{nucleotide}/
-    wrong_place ? raise(ArgumentError, "Wrong nucleotide in position #{(wrong_place + 1).to_s}") : strand
+    if wrong_place
+    then
+      raise(ArgumentError, "Wrong nucleotide in position #{(wrong_place + 1).to_s}")
+    end
   end
   
 end #class ComplementTest
