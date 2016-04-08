f698c69961fdfdd85a739b9112808d7ad581a5d2
Dmitri G. Brengauz <dmitri@momus.net>
2016-04-08 14:32:14 -0600

    All tests passing, time to make less ugly
    	modified:   elm/pangram/Pangram.elm

--- elm/pangram/Pangram.elm
+++ elm/pangram/Pangram.elm
@@ -1,1 +1,34 @@
-module Pangram where
+module Pangram (..) where
+
+import Regex exposing (regex)
+import String
+
+
+-- AFAIK, there's no way to generate a range of chars in Elm
+alphabet : List String
+alphabet =
+  [ "a", "b", "c", "d", "e", "f", "g", "h", "i"
+  , "j", "k", "l", "m", "n", "o", "p", "q", "r"
+  , "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+regexList : List String -> List Regex.Regex
+regexList listOfStrings =
+  List.map regex listOfStrings
+
+
+isPangram : String -> Bool
+isPangram sentence =
+  let
+    test_regex_list =
+      regexList alphabet
+    result_list =
+      List.map (flip Regex.contains (String.toLower sentence)) test_regex_list
+  in
+    if List.member False result_list then False
+    else True
+
+
+
+
+--    List.map (List.map Regex.contains test_regex_list)  sentence
