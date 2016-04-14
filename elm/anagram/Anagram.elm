c6c51a8dd8ff7cdd1071362e0fb34b0bc99aa987
Dmitri G. Brengauz <dmitri@momus.net>
2016-04-13 23:22:04 -0600

    First iteration of Anagram all green

--- elm/anagram/Anagram.elm
+++ elm/anagram/Anagram.elm
@@ -1,38 +1,67 @@
 module Anagram (..) where
 
+import String
+import Dict
 
 
 detect : String -> List String -> List String
 detect word wordList =
   List.filter (isAnagram word) wordList
 
-      
+
 isAnagram : String -> String -> Bool
 isAnagram wordOne wordTwo =
-  if wordOne == wordTwo then True
-  else False
+  let
+    one =
+      String.toLower wordOne
 
-      
-
-      
--- isAnagram -> String -> 
-
-
--- stringToCharDict : String -> Dict.Dict String Int
--- stringToCharDict string =
---   let
---     outDict = Dict.empty
---   in
---     case string of
---       String.isEmpty  ->
---         outstring
+    two =
+      String.toLower wordTwo
+  in
+    if one == two then
+      False
+    else
+      compareWords one two
 
 
+compareWords : String -> String -> Bool
+compareWords one two =
+  let
+    oneDict =
+      stringToCharDict one Dict.empty
+
+    twoDict =
+      stringToCharDict two Dict.empty
+  in
+    -- Can't == Dicts directly
+    if Dict.toList oneDict == Dict.toList twoDict then
+      True
+    else
+      False
 
 
+stringToCharDict : String -> Dict.Dict Char Int -> Dict.Dict Char Int
+stringToCharDict inString inDict =
+  let
+    stringTuple =
+      String.uncons inString
+  in
+    case stringTuple of
+      Nothing ->
+        inDict
 
-incrementEntry : Maybe number -> Maybe number     
-incrementEntry charecter  =
-  case charecter of
-    Nothing -> Nothing
-    Just value -> Just (value + 1)
+      Just ( char, string ) ->
+        if (Dict.member char inDict) then
+          stringToCharDict string (Dict.update char incrementEntry inDict)
+        else
+          stringToCharDict string (Dict.insert char 1 inDict)
+
+
+incrementEntry : Maybe number -> Maybe number
+incrementEntry number =
+  case number of
+    Nothing ->
+      Nothing
+
+    Just value ->
+      Just (value + 1)
