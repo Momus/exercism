502c24b6121ac049841bcf7e7acc87908787fab6
Dmitri G. Brengauz <dmitri@momus.net>
2016-04-21 20:57:20 -0600

    elm/word-count iteration 1 Ruby pangram I2

---
+++ elm/word-count/WordCount.elm
@@ -0,0 +1,80 @@
+module WordCount (..) where
+
+import String exposing (fromChar)
+import Dict
+import Regex exposing (regex)
+
+
+type alias CountDict =
+  Dict.Dict String Int
+
+
+wordCount : String -> CountDict
+wordCount phrase =
+  wCount phrase "" Dict.empty
+
+
+wCount : String -> String -> CountDict -> CountDict
+wCount phrase nextWord outDict =
+  let
+    phraseTuple =
+      phrase
+        |> String.toLower
+        |> String.uncons
+  in
+    case phraseTuple of
+      Nothing ->
+        if nextWord == "" then
+          outDict
+        else if Dict.member nextWord outDict then
+          Dict.update nextWord incrementEntry outDict
+        else
+          Dict.insert nextWord 1 outDict
+
+      Just ( firstChar, restString ) ->
+        case isGoodChar firstChar of
+          True ->
+            let
+              nextWord =
+                firstChar
+                  |> fromChar
+                  |> String.append nextWord
+            in
+              wCount restString nextWord outDict
+
+          False ->
+            let
+              isEmptyWord =
+                (nextWord == "")
+            in
+              case isEmptyWord of
+                True ->
+                  wCount restString "" outDict
+
+                False ->
+                  if Dict.member nextWord outDict then
+                    wCount restString "" (Dict.update nextWord incrementEntry outDict)
+                  else
+                    wCount restString "" (Dict.insert nextWord 1 outDict)
+
+
+isGoodChar : Char -> Bool
+isGoodChar test_c =
+  let
+    goodCharRegex =
+      "[a-z]|[0-9]"
+        |> regex
+  in
+    test_c
+      |> fromChar
+      |> Regex.contains goodCharRegex
+
+
+incrementEntry : Maybe number -> Maybe number
+incrementEntry number =
+  case number of
+    Nothing ->
+      Nothing
+
+    Just value ->
+      Just (value + 1)
--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,18 +1,17 @@
 class Pangram
 
   VERSION = 1
-  ALPHABET = ('a'..'z').to_a
+  ALPHABET = ('a'..'z')
   NUMBER_OF_LETTERS_IN_ALPHABET = 26
 
   def Pangram.is_pangram? str
 
     counter = 0
-    str.downcase!
+    comp_str = str.downcase
 
-    ALPHABET.each { |e| if str.include? e then (counter += 1) end }
+    counter = ALPHABET.count { |l| comp_str.include? l }
 
-    counter == NUMBER_OF_LETTERS_IN_ALPHABET ? true :  false
-
+    counter == NUMBER_OF_LETTERS_IN_ALPHABET
     
   end
 
