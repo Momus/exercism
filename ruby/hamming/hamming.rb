cfb920a60739c78aaa362ad399ba2a8baa285231
Dmitri G. Brengauz <dmitri@momus.net>
2016-04-24 20:45:38 -0600

    hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,37 +1,23 @@
 
-
-=begin
-module MicroScheme
-    
-  def r_car(list_like_object)
-    list_like_object[0]
-  end
-  
-  def r_cdr(list_like_object)
-    list_like_object = list_like_object[1..-1] if list_like_object
-  end
-
-end
-=end
-
 class Hamming
 
-  def self.compute(first_sequence, second_sequence)
-    result_string = "" 
-    #   include MicroScheme
-    if first_sequence.length  != second_sequence.length
-      raise ArgumentError, 'Hamming number only defined for sequences of same length'
-    else
-      loop  do
-        result_string =  first_sequence[0]  if first_sequence && first_sequence[0]  != second_sequence[0]
-        first_sequence[0]  = second_sequence[0]  = ""
-        break first_sequence.empty?
-      end 
-    end
-    
-    result_string.length
+  VERSION = 1
 
-  end #compute
+  def self.compute(first_sequence, second_sequence)
+
+    valid? first_sequence, second_sequence
+    
+    first_chars = first_sequence.chars
+    second_chars = second_sequence.chars
+
+    first_chars.count.each_index { |n|  first_sequence[n] != second_sequence[n] }
+
+  end
+
+  private
   
+  def self.valid? str1, str2
+    raise ArgumentError if str1.length != str2.length
+  end
   
-end #class Hamming
+end 
