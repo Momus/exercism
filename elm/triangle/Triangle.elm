400a6d95458dfddd8192ce6eee9f290dc8dd97d2
Dmitri G. Brengauz <dmitri@momus.net>
2016-09-08 15:01:12 -0600

    Updated triangle for 0.17

--- elm/triangle/Triangle.elm
+++ elm/triangle/Triangle.elm
@@ -1,22 +1,38 @@
-module Triangle (..) where
+module Triangle exposing (..)
+
+{-| Given: three numbers representing side lengths Determine if they
+can belong to a valid triangle. If they can return a Triangle type of
+the approporate kind. If not, return an error and a string explaining
+how the given inputs violate allowable length of triagle sides.
+
+triangleKind 2 2 4  == Ok Scaline
+triangleKind 1.0 0 0.3 == Err "Invalid lengths"
+
+-}
+
+import Result exposing (..)
 
 
---First make sure it's a triangle, then compute the kind.
-triangleKind : number -> number' -> number'' -> Result String String
+version : Int
+version =
+    2
+
+
+type Triangle
+    = Equilateral
+    | Isosceles
+    | Scalene
+
+
+triangleKind : number -> number -> number -> Result String Triangle
 triangleKind a b c =
-  if ((a <= 0) || (b <= 0) || (c <= 0)) then
-    Err "Invalid lengths"
-  else if (2 * (max a (max b c))) >= (a + (b + c)) then
-    Err "Violates inequality"
-  else
-    computeTriangleKind a b c
-
-
-computeTriangleKind : number -> number -> number -> Result b String
-computeTriangleKind a b c =
-  if ((a == b) && (b == c)) then
-    Ok "equilateral"
-  else if ((a == b) || (a == c) || (b == c)) then
-    Ok "isosceles"
-  else
-    Ok "scalene"
+    if ((a <= 0) || (b <= 0) || (c <= 0)) then
+        Err "Invalid lengths"
+    else if (2 * (max a (max b c))) >= (a + (b + c)) then
+        Err "Violates inequality"
+    else if ((a == b) && (b == c)) then
+        Ok Equilateral
+    else if ((a == b) || (a == c) || (b == c)) then
+        Ok Isosceles
+    else
+        Ok Scalene
--- elm/triangle/TriangleTests.elm
+++ elm/triangle/TriangleTests.elm
@@ -1,63 +1,49 @@
-module Main (..) where
+port module Main exposing (..)
 
-import Task
-import Console
-import ElmTest exposing (..)
-import Triangle exposing (triangleKind)
+import Test.Runner.Node exposing (run)
+import Json.Encode exposing (Value)
+import Test exposing (..)
+import Expect
+import Triangle exposing (triangleKind, Triangle(..))
 
 
 tests : Test
 tests =
-  suite
-    "triangleKind"
-    [ test
-        "equilateral triangles have equal sides"
-        (assertEqual (Ok "equilateral") (triangleKind 2 2 2))
-    , test
-        "larger equilateral triangles also have equal sides"
-        (assertEqual (Ok "equilateral") (triangleKind 10 10 10))
-    , test
-        "isosceles triangles have last two sides equal"
-        (assertEqual (Ok "isosceles") (triangleKind 3 4 4))
-    , test
-        "isosceles triangles have first and last sides equal"
-        (assertEqual (Ok "isosceles") (triangleKind 4 3 4))
-    , test
-        "isosceles triangles have two first sides equal"
-        (assertEqual (Ok "isosceles") (triangleKind 4 4 3))
-    , test
-        "isosceles triangles have in fact exactly two sides equal"
-        (assertEqual (Ok "isosceles") (triangleKind 10 10 2))
-    , test
-        "scalene triangles have no equal sides"
-        (assertEqual (Ok "scalene") (triangleKind 3 4 5))
-    , test
-        "scalene triangles have no equal sides at a larger scale too"
-        (assertEqual (Ok "scalene") (triangleKind 10 11 12))
-    , test
-        "scalene triangles have no equal sides at a larger scale too 2"
-        (assertEqual (Ok "scalene") (triangleKind 5 4 2))
-    , test
-        "very small triangles are legal"
-        (assertEqual (Ok "scalene") (triangleKind 0.4 0.6 0.3))
-    , test
-        "triangles with no size are illegal"
-        (assertEqual (Err "Invalid lengths") (triangleKind 0 0 0))
-    , test
-        "triangles with negative sides are illegal"
-        (assertEqual (Err "Invalid lengths") (triangleKind 3 4 -5))
-    , test
-        "triangles violating triangle inequality are illegal 1"
-        (assertEqual (Err "Violates inequality") (triangleKind 1 1 3))
-    , test
-        "triangles violating triangle inequality are illegal 2"
-        (assertEqual (Err "Violates inequality") (triangleKind 2 4 2))
-    , test
-        "triangles violating triangle inequality are illegal 3"
-        (assertEqual (Err "Violates inequality") (triangleKind 7 3 2))
-    ]
+    describe "triangleKind"
+        [ test "equilateral triangles have equal sides" <|
+            \() -> Expect.equal (Ok Equilateral) (triangleKind 2 2 2)
+        , test "larger equilateral triangles also have equal sides" <|
+            \() -> Expect.equal (Ok Equilateral) (triangleKind 10 10 10)
+        , test "isosceles triangles have last two sides equal" <|
+            \() -> Expect.equal (Ok Isosceles) (triangleKind 3 4 4)
+        , test "isosceles triangles have first and last sides equal" <|
+            \() -> Expect.equal (Ok Isosceles) (triangleKind 4 3 4)
+        , test "isosceles triangles have two first sides equal" <|
+            \() -> Expect.equal (Ok Isosceles) (triangleKind 4 4 3)
+        , test "isosceles triangles have in fact exactly two sides equal" <|
+            \() -> Expect.equal (Ok Isosceles) (triangleKind 10 10 2)
+        , test "scalene triangles have no equal sides" <|
+            \() -> Expect.equal (Ok Scalene) (triangleKind 3 4 5)
+        , test "scalene triangles have no equal sides at a larger scale too" <|
+            \() -> Expect.equal (Ok Scalene) (triangleKind 10 11 12)
+        , test "scalene triangles have no equal sides at a larger scale too 2" <|
+            \() -> Expect.equal (Ok Scalene) (triangleKind 5 4 2)
+        , test "very small triangles are legal" <|
+            \() -> Expect.equal (Ok Scalene) (triangleKind 0.4 0.6 0.3)
+        , test "triangles with no size are illegal" <|
+            \() -> Expect.equal (Err "Invalid lengths") (triangleKind 0 0 0)
+        , test "triangles with negative sides are illegal" <|
+            \() -> Expect.equal (Err "Invalid lengths") (triangleKind 3 4 -5)
+        , test "triangles violating triangle inequality are illegal 1" <|
+            \() -> Expect.equal (Err "Violates inequality") (triangleKind 1 1 3)
+        , test "triangles violating triangle inequality are illegal 2" <|
+            \() -> Expect.equal (Err "Violates inequality") (triangleKind 7 3 2)
+        ]
 
 
-port runner : Signal (Task.Task x ())
-port runner =
-  Console.run (consoleRunner tests)
+main : Program Value
+main =
+    run emit tests
+
+
+port emit : ( String, Value ) -> Cmd msg
