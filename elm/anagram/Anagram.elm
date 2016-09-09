524bb5ab631ce867554c218f4e1409efec461de9
Dmitri G. Brengauz <dmitri@momus.net>
2016-09-08 19:47:50 -0600

    0.17ed Anagram.elm

--- elm/anagram/Anagram.elm
+++ elm/anagram/Anagram.elm
@@ -1,4 +1,8 @@
-module Anagram (..) where
+module Anagram exposing (..)
+
+{-| Given: a word in the form of a string, and a list of strings
+Return: a sublist whose elemenst are anagrams of the input worldq
+-}
 
 import String
 import Dict
@@ -6,62 +10,66 @@ import Dict
 
 detect : String -> List String -> List String
 detect word wordList =
-  List.filter (isAnagram word) wordList
+    List.filter (isAnagram word) wordList
 
 
 isAnagram : String -> String -> Bool
 isAnagram wordOne wordTwo =
-  let
-    one =
-      String.toLower wordOne
+    let
+        one =
+            String.toLower wordOne
 
-    two =
-      String.toLower wordTwo
-  in
-    if one == two then
-      False
-    else
-      compareWords one two
+        two =
+            String.toLower wordTwo
 
+        oneDict =
+            stringToCharDict one Dict.empty
 
-compareWords : String -> String -> Bool
-compareWords one two =
-  let
-    oneDict =
-      stringToCharDict one Dict.empty
-
-    twoDict =
-      stringToCharDict two Dict.empty
-  in
-    -- Can't == Dicts directly
-    if Dict.toList oneDict == Dict.toList twoDict then
-      True
-    else
-      False
+        twoDict =
+            stringToCharDict two Dict.empty
+    in
+        -- a word is not an anagram of itself
+        if one == two then
+            False
+            -- can't == Dicts directly; compare lists of tuples
+        else if Dict.toList oneDict == Dict.toList twoDict then
+            True
+        else
+            False
 
 
+{-| Given a string, creates a Dict consisting of all the charecters in
+   the string mapped to the number of times each charecter occurs in the
+   string. Call with an empty Dict to start the tail wagging.
+
+   stringToCharDict "Jeremy Irons" Dict.empty == Dict.fromList
+   [('',1),('I',1),('J',1),('e',2),('m',1),('n',1),('o',1),('r',2),('s',1),('y',1)]
+   : Dict.Dict Char Int
+-}
 stringToCharDict : String -> Dict.Dict Char Int -> Dict.Dict Char Int
 stringToCharDict inString inDict =
-  let
-    stringTuple =
-      String.uncons inString
-  in
-    case stringTuple of
-      Nothing ->
-        inDict
+    let
+        stringTuple =
+            String.uncons inString
+    in
+        case stringTuple of
+            Nothing ->
+                inDict
 
-      Just ( char, string ) ->
-        if (Dict.member char inDict) then
-          stringToCharDict string (Dict.update char incrementEntry inDict)
-        else
-          stringToCharDict string (Dict.insert char 1 inDict)
+            Just ( char, string ) ->
+                if (Dict.member char inDict) then
+                    stringToCharDict string (Dict.update char incrementEntry inDict)
+                else
+                    stringToCharDict string (Dict.insert char 1 inDict)
 
 
+{-| Basically, (Maybe number)++
+-}
 incrementEntry : Maybe number -> Maybe number
 incrementEntry number =
-  case number of
-    Nothing ->
-      Nothing
+    case number of
+        Nothing ->
+            Nothing
 
-    Just value ->
-      Just (value + 1)
+        Just value ->
+            Just (value + 1)
--- elm/anagram/README.md
+++ elm/anagram/README.md
@@ -6,7 +6,16 @@ Given `"listen"` and a list of candidates like `"enlists" "google"
 "inlets" "banana"` the program should return a list containing
 `"inlets"`.
 
+## Getting Started
+
+To get started, please see docs on exercism.io:
+
+http://exercism.io/languages/elm
 
 ## Source
 
-Inspired by the Extreme Startup game [view source](https://github.com/rchatley/extreme_startup)
+Inspired by the Extreme Startup game [https://github.com/rchatley/extreme_startup](https://github.com/rchatley/extreme_startup)
+
+## Submitting Incomplete Problems
+It's possible to submit an incomplete solution so you can see how others have completed the exercise.
+
