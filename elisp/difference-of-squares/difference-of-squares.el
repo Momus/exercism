d188117c94519e1229e401661520d744da8a0b75
Dmitri G. Brengauz <dmitri@momus.net>
2017-03-05 19:01:23 -0700

    Elisp steaming ahead

--- elisp/difference-of-squares/difference-of-squares.el
+++ elisp/difference-of-squares/difference-of-squares.el
@@ -3,6 +3,29 @@
 ;;; Commentary:
 
 ;;; Code:
+(defun **2 (x)
+  "Square of X."
+  (* x x))
+
+
+(defun square-of-sums (x)
+  "X is sqaure-summmed."
+  (**2 (cl-loop for n
+             from 1 to x
+             sum n)))
+
+
+(defun sum-of-squares (x)
+  "X sum-squared."
+  (cl-loop for n
+           from 1 to x
+           sum (**2 n)))
+
+
+(defun difference (x)
+  "X differed."
+  (- (square-of-sums x)
+     (sum-of-sqaures x)))
 
 
 (provide 'difference-of-squares)
---
+++ elisp/gigasecond/gigasecond.el
@@ -0,0 +1,21 @@
+;;; gigasecond.el --- Gigasecond exercise (exercism)
+
+;;; Commentary:
+;; Calculate the date one gigasecond (10^9 seconds) from the
+;; given date.
+;;
+;; NB: Pay attention to  Emacs' handling of time zones and dst
+;; in the encode-time and decode-time functions.
+
+;;; Code:
+(defun from (list &rest zz)
+  "Do something with LIST.
+Also do ZZ."
+  '(40 46 1 10 9 1931))
+
+
+
+
+
+(provide 'gigasecond)
+;;; gigasecond.el ends here
