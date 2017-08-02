ee3bd3907404e8c00cce46eb912e06948fdad243
Dmitri <dmitri@melinoe>
2017-08-01 18:07:15 -0600

    siveing!!!

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -3,17 +3,27 @@ class Sieve
   attr_reader :primes
 
   def initialize(limit)
-    @primes = limit < 2 ? [] : sieve(limit)
+    if limit < 2
+      @primes = []
+    else
+      @primes = [2]
+      sieve(limit)
+    end
   end
 
   private
 
   def sieve(limit)
-    possible_primes = (2..limit).to_a
-    possible_primes
-  end
-
-  def pop_multiple(target, numeric_array)
-    numeric_arrray
+    ## Skiping  all even numbers
+    odds = (3..limit).step(2).to_a
+    until odds.size.zero?
+      last_prime = odds.shift
+      odds.each_index do |n|
+        unless odds[n].nil?
+          odds.delete_at(n) if (odds[n] % last_prime).zero?
+        end
+      end
+      @primes << last_prime
+    end
   end
 end
