4065a8d81e96d440d91c803d65cd5a42dd3a92f9
Dmitri <dmitri@momus.net>
2018-08-06 13:32:50 -0600

    emacs lisp two-fer version II

--- elisp/two-fer/two-fer.el
+++ elisp/two-fer/two-fer.el
@@ -4,9 +4,8 @@
 
 ;;; Code:
 (defun two-fer (&optional twoname)
-"The two-fer function using TWONAME."
-(if twoname (concat "One for " twoname ", one for me.")
-  '"One for you, one for me."))
+  "The two-fer function using TWONAME."
+  (concat "One for " (or twoname "you") ", one for me."))
 
 (provide 'two-fer)
 ;;; two-fer.el ends here
