c8404cb59f706cc34ea40d3915d2d5734539970f
Dmitri <dmitri@momus.net>
2018-08-14 19:22:48 -0600

    Rewrote using reduce

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,9 +1,6 @@
 (ns reverse-string)
 
 (defn reverse-string [string]
-  (loop [acc ""
-         strcoll (seq string)]
-    (if (empty? strcoll)
-      acc
-      (recur (str (first strcoll) acc)
-             (rest strcoll)))))
+  (if (empty? string)
+    ""
+    (reduce #(str %2 %1 ) string)))
