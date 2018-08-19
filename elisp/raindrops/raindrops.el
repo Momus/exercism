b312c2d14747408fbcdaf32f7bb3dde62db9b7ec
Dmitri <dmitri@momus.net>
2018-08-19 13:33:35 -0600

    Emacs Lisp Raindrops first iteration

--- elisp/raindrops/raindrops.el
+++ elisp/raindrops/raindrops.el
@@ -3,51 +3,15 @@
 ;;; Commentary:
 
 ;;; Code:
-
 (require 'cl)
 
-
-(defun drop-factor (n)
-  "Factor number, N, to convert to raindrops."
-  (let ((outvector [nil nil nil]))
-    (cond ((zerop (% n 3)) (aset outvector 0 (/ n 3)))
-          ((zerop (% n 7)) (aset outvector 1 (/ n 7)))
-          )
-    outvector))
-
-
-
 (defun convert (num)
-  "Convert integer N to its raindrops string."
-  (let ((outstring "baa"))
-    (progn (when (zerop (% n 3)) (concat  "Pling" outstring))
-     (when (zerop (% n 5)) (concat  "Plang" outstring))
-     (when (zerop (% n 7)) (concat  "Plong" outstring))
-     )
-    outstring))
-
-(convert 7)
-
-
-
-
-
-(defun wtf (n)
-  (let ((stx "xx"))
-    (progn (when (= (% n 3)) (concat stx "Pling"))
-     (when (= (% n 5)) (concat stx "Plang")))))
-
-(% 6 3)
-
-(wtf 3)
-
-(defun drop-test ())
+  "Convert integer NUM to its raindrops string."
+  (let ((outstring ""))
+    (progn (when (zerop (% num 3)) (setf outstring (concat  outstring "Pling")))
+           (when (zerop (% num 5)) (setf outstring (concat  outstring "Plang")))
+           (when (zerop (% num 7)) (setf outstring (concat  outstring "Plong")))
+           (if (equal "" outstring) (number-to-string num) outstring))))
 
 (provide 'raindrops)
 ;;; raindrops.el ends here
-
-
-
-(emptyp "")
-
-(let ((a 3)) (+ a 1))
