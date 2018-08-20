87556d340db8485f5edad3eeea391c95bba7bc65
Dmitri <dmitri@melinoe>
2018-08-20 09:48:12 -0600

    started on Ruby clock

---
+++ ruby/clock/clock.rb
@@ -0,0 +1,18 @@
+## A new kind of clock in Ruby.
+class Clock
+  def initialize(hour: 0, minute: 0)
+    @hour = hour
+    @minute = minute
+  end
+
+  def to_s
+    @hour.to_s.rjust(2, '0') + ':' +
+      @minute.to_s.rjust(2, '0')
+  end
+
+  def +() end
+
+  def -() end
+
+  def ==()end
+end
