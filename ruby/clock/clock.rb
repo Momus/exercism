24cb3a1992b8ebe0c5e6441e8b765e9738780f98
Dmitri <dmitri@melinoe>
2018-08-22 15:20:17 -0600

    Ruby Clock iteration 1

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,18 +1,48 @@
-## A new kind of 24 hour clock in Ruby.
+## Horas non numero nisi serenas
 class Clock
+  MINUTES_IN_A_DAY = 1440
+
   def initialize(hour: 0, minute: 0)
-    @hour = hour
-    @minute = minute
+    @clock = (make_mins(hour) + minute) % MINUTES_IN_A_DAY
   end
 
   def to_s
-    @hour.to_s.rjust(2, '0') + ':' +
-      @minute.to_s.rjust(2, '0')
+    hhmm_format(@clock)
   end
 
-  def +() end
+  ## This seems like a rare instance of where possibly breaking
+  ## encapsulation by using .instance_variable_get may be justified,
+  ## since an accessor method would break the API, and we're dealing
+  ## with objects of the same class as part of the definition of that
+  ## class.
+  def +(other)
+    hhmm_format((@clock +
+                 other.instance_variable_get(:@clock)) %
+                MINUTES_IN_A_DAY)
+  end
 
-  def -() end
+  def -(other)
+    hhmm_format((@clock -
+                 other.instance_variable_get(:@clock)) %
+                MINUTES_IN_A_DAY)
+  end
 
-  def ==()end
+  def ==(other)
+    @clock == other.instance_variable_get(:@clock)
+  end
+
+  private
+
+  def make_mins(hours)
+    hours * 60
+  end
+
+  def hhmm_format(minutes)
+    (minutes / 60).to_s.rjust(2, '0') << ':' <<
+      (minutes % 60).to_s.rjust(2, '0')
+  end
+end
+
+module BookKeeping
+  VERSION = 3
 end
