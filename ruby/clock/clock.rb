dd2b91b199f43c9b2f1fd3524c1cb54cdca3d8c5
Dmitri <dmitri@momus.net>
2018-08-27 20:28:28 -0600

    clock.rb approved!

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -2,15 +2,15 @@
 class Clock
   MINUTES_IN_A_DAY = 1440
 
+  BABYLONIAN_BASE = 60
+
   def initialize(hour: 0, minute: 0)
     @clock = ((hour * 60) + minute) %
              MINUTES_IN_A_DAY
   end
 
   def to_s
-    (@clock / 60).to_s.rjust(2, '0') <<
-      ':' <<
-      (@clock % 60).to_s.rjust(2, '0')
+    format('%02d:%02d', hours, minutes)
   end
 
   def +(other)
@@ -24,13 +24,29 @@ class Clock
   end
 
   def ==(other)
-    @clock ==
-      other.clock
+    self.class == other.class &&
+      @clock == other.clock
+  end
+
+  alias eql? ==
+
+  def hash
+    @clock.hash
   end
 
   protected
 
   attr_reader :clock
+
+  private
+
+  def minutes
+    @clock % BABYLONIAN_BASE
+  end
+
+  def hours
+    @clock / BABYLONIAN_BASE
+  end
 end
 
 module BookKeeping
