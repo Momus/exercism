1b47b8ec57a47b3c240a822ca124ac3c3fbc8ab2
Dmitri <dmitri@melinoe>
2018-09-03 13:16:45 -0600

    minor chages to Ruby Scrabble.

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,13 +1,13 @@
 #! /usr/bin/env ruby
 
-## Scoring Scrabble using a modifier array representing board tiles:
+## Scoring Scrabble using a modifier array representing the tile setup:
 ##         K         W    I   J   I        B          O        S
 ## [[:DW, "shook"], nil, nil, 0, nil, [:DL, "bale"], nil, [:TW, "sincerity"]]
 ##
 class Scrabble
-  US_TILE_SCORES = { a: 1, b: 3, c: 3, d: 2, e: 1, f: 4, g: 2, h: 4, i: 1,
-                     j: 8, k: 5, l: 1, m: 3, n: 1, o: 1, p: 3, q: 10, r: 1,
-                     s: 1, t: 1, u: 1, v: 4, w: 4, x: 8, y: 4, z: 10 }.freeze
+  EN_US_TILES = { a: 1, b: 3, c: 3, d: 2, e: 1, f: 4, g: 2, h: 4, i: 1,
+                  j: 8, k: 5, l: 1, m: 3, n: 1, o: 1, p: 3, q: 10, r: 1,
+                  s: 1, t: 1, u: 1, v: 4, w: 4, x: 8, y: 4, z: 10 }.freeze
 
   MAX_TILES_PER_PLAY = 7
 
@@ -18,16 +18,23 @@ class Scrabble
     @modifiers = modifiers
   end
 
-  class << self
-    def score(word, _modifiers = [])
-      return 0 if word.nil?
-      word.chars.reduce(0) { |acc, elem| acc + points(elem.downcase) }
-    end
+  def score(word, _modifiers = [])
+    return 0 if word.nil?
+    word.chars.reduce(0) { |acc, elem| acc + points(elem.downcase) }
+  end
 
+  protected
+
+  def raw_score(word)
+    return 0 if word.nil?
+    word.chars.reduce(0) { |acc, elem| acc + points(elem.downcase) }
+  end
+
+  class << self
     private
 
     def points(char)
-      US_TILE_SCORES[char.to_sym] || 0
+      EN_US_TILES[char.to_sym] || 0
     end
   end
 end
