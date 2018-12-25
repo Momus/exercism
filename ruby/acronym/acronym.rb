1447858cd75e6ce88826c78bf281c71824163eaa
Dmitri <dmitri@momus.net>
2018-12-24 18:28:15 -0700

    Spelling cleanup

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,13 +1,13 @@
 ## Provides the abbreviate class method.
 class Acronym
-  INITAL_LETTER = /(?: # Exclude first capture group from match
-  ^|\s|[[:punct:]])    # start, whitespace charecter, or punctuation
-  ([a-zA-Z])           # Capture the next alphabetic numeric charecter.
-  /x                   # Allow comments and whitespace in the regex.
+  INITIAL_LETTER = /(?: # exclude first capture group from match
+  ^|\s|[[:punct:]])     # string start, white space, or punctuation
+  ([a-zA-Z])            # capture the next alphabetic character.
+  /x                    # allow comments and white space in regex.
 
   class << self
     def abbreviate(phrase)
-      phrase.scan(INITAL_LETTER).join.upcase
+      phrase.scan(INITIAL_LETTER).join.upcase
     end
   end
 end
