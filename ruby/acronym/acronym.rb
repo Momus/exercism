49a9274f699004e28a6debf0d45ab6485b1ef7d1
Dmitri <dmitri@melinoe>
2018-12-25 11:25:54 -0700

    Ruby Acroym iteration 2

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,10 +1,6 @@
 ## Provides the abbreviate class method.
 class Acronym
-  INITIAL_LETTER = /(?: # exclude first capture group from match
-  ^|\s|[[:punct:]])     # string start, white space, or punctuation
-  ([a-zA-Z])            # capture the next alphabetic character.
-  /x                    # allow comments and white space in regex.
-
+  INITIAL_LETTER = /\b[a-zA-Z]/
   class << self
     def abbreviate(phrase)
       phrase.scan(INITIAL_LETTER).join.upcase
