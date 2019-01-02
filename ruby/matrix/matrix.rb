e63030ea2a2ad7a31889098109c0b80cf7cf0ebf
Dmitri <dmitri@momus.net>
2019-01-01 20:13:49 -0700

    Ruby Matrix 2.0 (NOW! with benchmark!)

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,8 +1,17 @@
+require 'benchmark'
+
 ## Exerecise to impliment my own transpose method for Ruby arrays.
 module MyTranspose
   refine Array do
     def my_transpose
-      transpose
+      num_cols = self[0].length
+      column_matrix = Array.new(num_cols) { Array.new(length) }
+      length.times do |r|
+        num_cols.times do |c|
+          column_matrix[c][r] = self[r][c]
+        end
+      end
+      column_matrix
     end
   end
 end
@@ -28,3 +37,29 @@ class Matrix
     rows.my_transpose
   end
 end
+
+## Benchmark test of my_transpose
+randarr = Array.new(1000) { Array.new(1000) { rand(1000) } }
+my_transpose_time = Benchmark.measure do
+  using MyTranspose
+  500.times do
+    randarr.my_transpose
+  end
+end
+puts 'my transpose:'
+puts my_transpose_time
+
+transpose_time = Benchmark.measure do
+  using MyTranspose
+  500.times do
+    randarr.transpose
+  end
+end
+
+puts 'Array::transpose:'
+puts transpose_time
+
+## my transpose:
+##  71.900000   0.430000  72.330000 ( 72.648392)
+## Array::transpose:
+##  14.250000   0.270000  14.520000 ( 14.596483)
