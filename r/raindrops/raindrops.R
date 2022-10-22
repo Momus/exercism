82660f634a1c119bcfd681ece03bd59c2f60b1d1
Dmitri Brengauz <dmitri@Eos.momus.org>
2022-10-22 16:52:55 -0600

    r/raindrops complete

--- r/raindrops/raindrops.R
+++ r/raindrops/raindrops.R
@@ -1,14 +1,12 @@
 raindrops <- function(number) {
   rain = ""
   if (number %% 3 == 0) {
-      rain = paste(rain, "Pling", sep="")}
+      rain <- paste(rain, "Pling", sep="")}
   if (number %% 5 == 0) {
-      rain = paste(rain, "Plang", sep="")}
+      rain <- paste(rain, "Plang", sep="")}
   if (number %% 7 == 0) {
-      rain = paste(rain, "Plong", sep="")
-  }
-  else 
-      rain = as.character(number)
+      rain <- paste(rain, "Plong", sep="")}
+  if (rain == "") {
+      rain <- as.character(number)}
   rain
 }
-
