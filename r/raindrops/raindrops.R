1a8ae9fbf6768144d9798ed865f991c487866527
Dmitri Brengauz <dmitri@Eos.momus.org>
2022-10-22 15:48:01 -0600

    doing it the hard way

--- r/raindrops/raindrops.R
+++ r/raindrops/raindrops.R
@@ -1,13 +1,14 @@
 raindrops <- function(number) {
   rain = ""
-  if (number %% 3 == 0){
-    paste(rain, "Pling", sep="")}
-  else if (number %% 5 == 0) {
-    paste(rain, "Plang", sep="")}
-  else if (number %% 7 == 0) {
-    paste(rain, "Plong", sep="")
+  if (number %% 3 == 0) {
+      rain = paste(rain, "Pling", sep="")}
+  if (number %% 5 == 0) {
+      rain = paste(rain, "Plang", sep="")}
+  if (number %% 7 == 0) {
+      rain = paste(rain, "Plong", sep="")
   }
   else 
-    as.character(number)
-  #"Pling"
+      rain = as.character(number)
+  rain
 }
+
