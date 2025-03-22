d5e6b5edd3fcba9dfdebfc2e9cc44e909416b9d9
Dmitri Brengauz <dmitri@momus.net>
2025-03-22 12:39:50 -0600

    finished python bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,16 @@
-def response(hey_bob):
-    pass
+from typing import Any
+
+def response(hey_bob: Any) -> str :
+    """Surley responses from Bob"""
+    
+    match hey_bob:
+        case str() as reply if not reply.strip():
+            return "Fine. Be that way!"
+        case str() as reply if reply.isupper() and reply.rstrip().endswith("?"):
+            return "Calm down, I know what I'm doing!" 
+        case str() as reply if reply.isupper():
+            return "Whoa, chill out!"
+        case str() as reply if reply.rstrip().endswith("?"):
+            return "Sure." 
+        case _:
+            return "Whatever."
