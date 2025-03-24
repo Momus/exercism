2a1a9f889d43e1343a288dac79972474e495f7a6
Dmitri Brengauz <dmitri@momus.net>
2025-03-24 14:32:31 -0600

    python isbn verifier first solution

---
+++ python/isbn-verifier/isbn_verifier.py
@@ -0,0 +1,26 @@
+def is_valid(isbn):
+    total = 0
+    multiple = 10
+    try:
+        check_char = isbn[-1]
+    except IndexError:
+        return False
+    if check_char == "X":
+        total += 10
+    else:
+        try:
+            total += int(check_char)
+        except ValueError:
+            return False
+    for digit in isbn[:-1]:
+        if digit == "-":
+            continue
+        try:
+            total += int(digit) * multiple
+            multiple -= 1
+            if multiple < 1: #get out early if input too long
+                return False
+        except ValueError:
+            return False
+
+    return total % 11 == 0 and multiple == 1
