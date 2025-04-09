ff4a8469a47410e141fa9d0e6ce48ad62e16f7f0
Dmitri Brengauz <dmitri@momus.net>
2025-04-08 18:31:00 -0600

    half of python/all-your-base works

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -1,2 +1,20 @@
-def rebase(input_base, digits, output_base):
+'''all your base'''
+
+def from_base(input_base: int, number: int) -> int:
+    ''' base ten int from base, number in base'''
+    num_list: list[str] = list(str(number))
+    num_list.reverse()
+    out: int = 0
+    for power, digit in enumerate(num_list):
+        if int(digit) > input_base:
+            raise ValueError("all digits must satisfy 0 <= d < input base")
+        out += (int(input_base ** power)) * int(digit)
+    return out
+
+def to_base(output_base: int, number: int) -> int:
     pass
+
+
+def rebase(input_base: int, digits: list[int], output_base: int) -> list[str]:
+    '''belong to us'''
+    return [to_base(output_base, from_base(input_base, num)) for num in digits]
