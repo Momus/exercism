2c0a33feb3a371daa54b8e5afadadb5cbfcaa04b
Dmitri Brengauz <dmitri@momus.net>
2025-05-25 08:36:49 -0600

    python binary-search v2

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -4,21 +4,18 @@ def find(search_list, value):
     '''find a  value in an array and return the position'''
     def r_find(search_list, value, left, right):
         mid = ((right - left) // 2) + left
-        print("mid=", mid, " right=", right, " left=", left)
         if not search_list:
             raise ValueError("value not in array")
         if search_list[mid] == value:
             return mid
         if search_list[mid + 1] == value:
-            return mid +1
-        if (right - left) in (0,1):
+            return mid + 1
+        if right - left in (0,1):
             raise ValueError("value not in array")
         if search_list[mid] < value:
             left = mid
-            print("righ:", right)
         else:
             right = mid
-            print("left:", left)
         return r_find(search_list, value, left, right)
-    
+
     return r_find(search_list, value, 0, len(search_list) -1)
