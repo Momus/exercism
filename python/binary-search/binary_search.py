cd7e504c4be5bc56cf2a724fc9f08961847ccb2f
Dmitri Brengauz <dmitri@momus.net>
2025-05-25 08:43:50 -0600

    python binary-search first submission

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,21 +1,20 @@
 '''simple binary search implementation'''
 
-def find(search_list, value):
-    '''find a  value in an array and return the position'''
-    def r_find(search_list, value, left, right):
-        mid = ((right - left) // 2) + left
-        if not search_list:
-            raise ValueError("value not in array")
-        if search_list[mid] == value:
-            return mid
-        if search_list[mid + 1] == value:
-            return mid + 1
-        if right - left in (0,1):
-            raise ValueError("value not in array")
-        if search_list[mid] < value:
-            left = mid
-        else:
-            right = mid
-        return r_find(search_list, value, left, right)
-
-    return r_find(search_list, value, 0, len(search_list) -1)
+def find(search_list, value, left=0, right=None):
+    '''find a  value in an array and return position if present'''
+    if not search_list:
+        raise ValueError("value not in array")
+    if not right:
+        right = len(search_list) - 1
+    mid = ((right - left) // 2) + left
+    if search_list[mid] == value:
+        return mid
+    if search_list[mid + 1] == value:
+        return mid + 1
+    if right - left in (0,1):
+        raise ValueError("value not in array")
+    if search_list[mid] < value:
+        left = mid
+    else:
+        right = mid
+    return find(search_list, value, left, right)
