e0f46c8a215bde4a7e18b80330c9d9d9e30cdca1
Dmitri Brengauz <dmitri@momus.net>
2025-05-27 09:18:26 -0600

    python wordify download

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,20 +1,27 @@
 '''simple binary search implementation'''
 
-def find(search_list, value, left=0, right=None):
-    '''find a  value in an array and return position if present'''
-    if not search_list:
-        raise ValueError("value not in array")
-    if not right:
-        right = len(search_list) - 1
-    mid = ((right - left) // 2) + left
-    if search_list[mid] == value:
-        return mid
-    if search_list[mid + 1] == value:
-        return mid + 1
-    if right - left in (0,1):
-        raise ValueError("value not in array")
-    if search_list[mid] < value:
-        left = mid
-    else:
-        right = mid
-    return find(search_list, value, left, right)
+def find(search_list: list[int], value: int) -> int:
+    '''Find a  value in an array and return position if present
+    
+    Parameters:
+         search_list: list of integers to be searched sorted in ascending
+                      order
+         value: an integer to be found in the list
+
+    Returns:
+          integer corresponding to the location in the array if found.
+          Raise ValueError if not found.
+    '''
+    left, right = 0, len(search_list) - 1
+
+    while left <= right:
+        mid = (right + left) // 2
+        mid_val = search_list[mid]
+        if mid_val == value:
+            return mid
+        if value < mid_val:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    raise ValueError("value not in array")
