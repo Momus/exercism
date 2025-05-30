ca559c6a14a794330a4b7d9f29218a2d32027da6
Dmitri Brengauz <dmitri@momus.net>
2025-05-30 12:39:18 -0600

    python secret-hanshake ok enough: not really undrestaning binary ops

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,2 +1,12 @@
+ACTION = ["wink", "double blink", "close your eyes", "jump"]
+
 def commands(binary_str):
-    pass
+    comms = [int(x) for x in list(binary_str)]
+    comms.reverse()
+    seq = []
+    for el in range(len(comms) - 1):
+        if comms[el]:
+            seq.append(ACTION[el])
+    if comms[-1]:
+        seq.reverse()
+    return seq
